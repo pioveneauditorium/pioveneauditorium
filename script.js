8cb0ea690168fbec30b9b0fc2a5a9a52ded23836
pioveneauditorium <rossoshocking@gmail.com>
2025-10-12 14:33:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
           item.className = 'carousel-item';
 
           const img = document.createElement('img');
+          img.loading = "lazy"; // ✅ attiva il lazy loading
           img.src = immagine;
           img.alt = titolo;
 
